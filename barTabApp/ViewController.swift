836109d05be3837cac2a4482272aab8d6a66b8b6
Owen Harper <owenharper@Owens-MacBook-Pro.local>
2017-01-18 01:12:35 +0000

    Implemented post method to show how to post to firebase and date/time method.

--- barTabApp/ViewController.swift
+++ barTabApp/ViewController.swift
@@ -16,6 +16,47 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    
+    
+    
+    
+    /**
+     Test post: stores defined variables title, message and time under "Table" + currentTableNumber/currentTime as "title", "message" and "timeField"
+     */
+    func post(){
+        
+        let currentTime = getDateTime()
+        let currentTableNumber = "1"
+        let title = "Title"
+        let message = "Message"
+        let time = "12:23"
+        
+        let order : [String : String] = ["title" : title, "message" : message, "timeField" : time]
+        
+        let databaseRef = FIRDatabase.database().reference()
+        
+        databaseRef.child("Table"+currentTableNumber).child(currentTime).setValue(order)
+        
+    }
+    
+    /**
+     Gets current time and returns it as a string in the form dd-MM-yy HH:mm
+     */
+    func getDateTime() -> String {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MM-yy HH:mm"
+        let result = formatter.string(from: date)
+        Swift.print("TIME"+result)
+        return result
+    }
+
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
