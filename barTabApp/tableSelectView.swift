56fd41c28cc98acf075c7de0d37e4dfd34e87eb3
Owen Harper <owenharper@owens-mbp.lan>
2017-01-23 20:14:07 +0000

    Setup table number entry

---
+++ barTabApp/tableSelectView.swift
@@ -0,0 +1,24 @@
+//
+//  tableSelectView.swift
+//  barTabApp
+//
+//  Created by Owen Harper on 21/01/2017.
+//  Copyright © 2017 Owen Harper. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class tableSelectView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
